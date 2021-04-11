9819f2fea15b480ad2e12eee7a6f4786aec31623
Georghe Julca <petergeorghe.julca@enti.cat>
2021-04-11 12:15:24 +0200

    Main Scene
    Tile Palette Setup

---
+++ GheoMann/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SoundManager
+{
+    public static void PlaySound()
+    {
+        GameObject soundGameObject = new GameObject("Sound");
+        AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
+
+        audioSource.PlayOneShot(GameAssets.i.playerMovement);
+    }
+    // Start is called before the first frame update
+}
